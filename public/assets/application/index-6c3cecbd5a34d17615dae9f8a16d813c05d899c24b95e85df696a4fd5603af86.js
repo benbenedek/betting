d6a1f9abb19fc93c80552a1466ffd52841bae928
Ben Benedek <ben.benedek@fiverr.com>
2015-09-19 17:05:39 +0300

    ....

--- public/assets/application/index-6c3cecbd5a34d17615dae9f8a16d813c05d899c24b95e85df696a4fd5603af86.js
+++
@@ -1,8 +0,0 @@
-$(document).on('page:load',
-     function(){
-          $("a#fixture_ajax_trigger").bind("ajax:success",
-                   function(evt, data, status, xhr){
-                        //this assumes the action returns an HTML snippet
-                        $("div#fixture").html(data);
-           });
-});
