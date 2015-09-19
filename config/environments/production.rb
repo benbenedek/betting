478773ca57009db838a8e57395f23d1129552836
Ben Benedek <ben.benedek@fiverr.com>
2015-09-19 17:40:55 +0300

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,6 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_assets = false
 end
