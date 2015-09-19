879f47f662abe08864b7a5aa5d3bd6dd1e7f22cb
Ben Benedek <ben.benedek@fiverr.com>
2015-09-19 17:37:32 +0300

    configuration

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,5 @@ Bundler.require(*Rails.groups)
 module Betting
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.serve_static_files = false
-    config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
   end
 end
