4be46603802905e05bce075f4ad84d5ae7897b3b
Ben Benedek <ben.benedek@fiverr.com>
2015-09-19 16:36:50 +0300

    Precompiled assets for bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile =  ['*.js']
 
 end
