3d9493741eb1b0ef22a1c4fc0e47d3bdc23ba2a1
Ben Benedek <ben.benedek@fiverr.com>
2015-09-20 23:27:24 +0300

    Minor fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
       # Create an error message.
-      flash[:danger] = 'Invalid name/password combination' # Not quite right!
+      flash[:danger] = 'משתמש וסיסמא לא נכונים' # Not quite right!
       render 'new'
     end
   end
