a73af6482f1512a8ff8e18f0bfa64ae083c9f93b
Ben Benedek <ben.benedek@fiverr.com>
2018-09-24 17:50:34 -0400

    [FIX] Links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,12 +29,16 @@ module ApplicationHelper
   end
 
   def link_to_next_fixture(fixture)
-    link_to "מחזור הבא", index_path(:number => fixture.number + 1), :class=>"loader-ajax-link"
+    link_to "מחזור הבא", link_to_league_fixture(fixture.league_id, fixture.number + 1), :class=>"loader-ajax-link"
   end
 
   def link_to_prev_fixture(fixture)
     return if fixture.number < 2
-    link_to "מחזור הקודם", index_path(:number => fixture.number - 1 ), :class=>"loader-ajax-link"
+    link_to "מחזור הקודם", link_to_league_fixture(fixture.league_id, fixture.number - 1), :class=>"loader-ajax-link"
+  end
+
+  def link_to_league_fixture(league_id, fixture_id)
+    index_path(league_id: league_id, number: fixture_id)
   end
 
   def get_score_table
