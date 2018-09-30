9483b415fd0c68983cba291b6ada6e01affec804
Ben Benedek <ben.benedek@fiverr.com>
2018-09-30 09:32:32 -0400

    [FIX] Change datetime to timestamp for PG compliance

--- db/migrate/20180930114156_add_match_date_and_odds.rb
+++ db/migrate/20180930114156_add_match_date_and_odds.rb
@@ -1,6 +1,6 @@
 class AddMatchDateAndOdds < ActiveRecord::Migration
   def change
-    add_column :matches, :date, :DateTime
+    add_column :matches, :date, :timestamp
     add_column :matches, :home_odds, :float
     add_column :matches, :away_odds, :float
   end
