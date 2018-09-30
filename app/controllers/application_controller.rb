333462419ebac400b4c10ba2cf8cf61b11f69765
Ben Benedek <ben.benedek@fiverr.com>
2018-09-30 08:24:57 -0400

    [FEAT] Add league name/season to score table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,7 @@ class ApplicationController < ActionController::Base
   def scoretable
     redirect_to login_path and return unless logged_in?
     league_id = params[:league_id]
+    @league = League.find(league_id)
     @results = get_score_table(league_id)
   end
 
