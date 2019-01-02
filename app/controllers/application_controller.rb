759490f3b0e4d334f0bdc2eb532c1f513784f360
Ben Benedek <ben.benedek@fiverr.com>
2019-01-02 11:42:07 -0500

    [FEAT] Implement simple remember me with auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,16 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery :except => :fixture
 
+  before_action :check_logged_in
+
+  def check_logged_in
+    return if logged_in?
+    return unless cookies.signed[:user_id].present? && cookies.signed[:auth_token].present?
+
+    user = User.where(id: cookies.signed[:user_id], auth_token: cookies.signed[:auth_token]).first
+    log_in(user) if user.present?
+  end
+
   def index
     redirect_to login_path and return unless logged_in?
     @fixture = params[:number].present? ?
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, length: { minimum: 6 }
-  
+
+  def generate_token!
+    self.auth_token = SecureRandom.urlsafe_base64
+    self.save(validate: false)
+  end
 end
