2099de6e823089145ba4a5bf8736e8811826d35a
Ben Benedek <benbenedek@gmail.com>
2019-09-16 05:22:04 -0400

    Add chellenge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.compile = false
   config.assets.precompile =  ['*.js']
 end
